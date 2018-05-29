1552c1164e4ae2253c6a5179d643bdf7e533984f
pjm77 <probably.just.me77@gmail.com>
2018-05-30 01:23:07 +0200

    Numbers_Guessing2 basic design ready

--- src/application/Numbers_Guessing2.java
+++ src/application/Numbers_Guessing2.java
@@ -1,6 +1,12 @@
 package application;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Numbers_Guessing2 extends Application {
@@ -11,6 +17,30 @@ public class Numbers_Guessing2 extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
+		primaryStage.setTitle("Guess Numbers #2");
+		VBox root = new VBox(5);
+		Label upperLab = new Label("Think of a number between 0 and 1000.");
+		Label middleUpperLab = new Label("I bet I can guess it in no more than 10 attempts!");
+		Label middleLowerLab = new Label();
+		Label lowerLab = new Label();
+		TilePane tile = new TilePane();
+		tile.setPrefColumns(3);
+		Button button1 = new Button("  TOO MUCH  ");
+		Button button2 = new Button("  CORRECT  ");
+		Button button3 = new Button(" NOT ENOUGH ");
+		root.setAlignment(Pos.CENTER);
+		tile.getChildren().add(button1);
+		tile.getChildren().add(button2);
+		tile.getChildren().add(button3);
+		
+		
+		root.getChildren().add(upperLab);
+		root.getChildren().add(middleUpperLab);
+		root.getChildren().add(middleLowerLab);
+		root.getChildren().add(lowerLab);
+		root.getChildren().add(tile);
+		Scene scene = new Scene(root,300,150);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 }
