6aa9ae82ff0aecc00aee4cc85c8f2bc6a47ec189
pjm77 <probably.just.me77@gmail.com>
2018-06-07 02:42:34 +0200

    some UI elements added in Dice

--- src/application/Dice.java
+++ src/application/Dice.java
@@ -5,8 +5,12 @@ import java.util.Random;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class Dice extends Application {
@@ -31,9 +35,24 @@ public class Dice extends Application {
 		};
 		
 		GridPane root = new GridPane();
+		Label label1 = new Label("Multiplier");
+//		label1.setStyle("-fx-border-color: black");
+		TextField multiplier = new TextField();
+		multiplier.setPrefSize(50, 50);
+		Button button1 = new Button("+");
+		Button button2 = new Button("-");
+		root.add(label1, 0, 0, 2, 1);
+		root.add(multiplier, 0, 1);
+		root.add(button1, 0, 2);
+		root.add(button2, 1, 2);
+		GridPane.setColumnSpan(label1, 2);
+		GridPane.setHalignment(label1, HPos.CENTER);
+		GridPane.setColumnSpan(multiplier, 2);
+		
 		Scene scene = new Scene(root,400,400);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
