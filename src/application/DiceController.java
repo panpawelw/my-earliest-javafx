0f3e8f959a7bcf08f88f2f0a26d996562ed7c724
pjm77 <probably.just.me77@gmail.com>
2018-06-13 00:37:00 +0200

    dice type selection finished

--- src/application/DiceController.java
+++ src/application/DiceController.java
@@ -2,11 +2,11 @@ package application;
 
 import java.util.Random;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 
@@ -27,19 +27,27 @@ public class DiceController extends GridPane{
 	public TextField modifierTF;
 	public Button modifierPlusB;
 	public Button modifierMinusB;
-	
-	public void myGroupAction(ActionEvent action)
-    {
-      System.out.println("Toggled: " + diceTypeTG.getSelectedToggle().getUserData().toString());
-    }
+	public ToggleButton tBD3;
+	public ToggleButton tBD4;
+	public ToggleButton tBD6;
+	public ToggleButton tBD8;
+	public ToggleButton tBD10;
+	public ToggleButton tBD12;
+	public ToggleButton tBD20;
+	public ToggleButton tBD100;
 	
 	@FXML
 	void initialize() {
 		multiplierL.setUserData(1);
 		modifierL.setUserData(0);
-//		diceTypeTG.
-		
-		
+		tBD3.setUserData(3);
+		tBD4.setUserData(4);
+		tBD6.setUserData(6);
+		tBD8.setUserData(8);
+		tBD10.setUserData(10);
+		tBD12.setUserData(12);
+		tBD20.setUserData(20);
+		tBD100.setUserData(100);
 	}
 	
 	public void handleRollButton() {
@@ -109,5 +117,9 @@ public class DiceController extends GridPane{
 		modifier--;
 		modifierTF.setText("" + modifier);
 		modifierL.setUserData(modifier);
+	}
+	
+	public void handleToggleButtons() {
+		diceTypeL.setUserData(diceTypeTG.getSelectedToggle().getUserData());
 	}
 }
