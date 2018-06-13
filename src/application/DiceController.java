f3bda0dc8a63bc8f8215b7611f3e7328f100d062
pjm77 <probably.just.me77@gmail.com>
2018-06-13 02:45:57 +0200

    add invalid parameters exceptions handling

--- src/application/DiceController.java
+++ src/application/DiceController.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Random;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -38,8 +40,9 @@ public class DiceController extends GridPane{
 	
 	@FXML
 	void initialize() {
-		multiplierL.setUserData(1);
-		modifierL.setUserData(0);
+		multiplierL.setUserData((long) 1);
+		modifierL.setUserData((long) 0);
+		diceTypeL.setUserData(0);
 		tBD3.setUserData(3);
 		tBD4.setUserData(4);
 		tBD6.setUserData(6);
@@ -48,25 +51,50 @@ public class DiceController extends GridPane{
 		tBD12.setUserData(12);
 		tBD20.setUserData(20);
 		tBD100.setUserData(100);
+		multiplierTF.focusedProperty().addListener(new ChangeListener<Boolean>() {
+		    @Override
+		    public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+		        if(!newValue) {
+		            handleMultiplierTF();
+		        }
+		    }
+		});
+		modifierTF.focusedProperty().addListener(new ChangeListener<Boolean>() {
+		    @Override
+		    public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+		        if(!newValue) {
+		            handleModifierTF();
+		        }
+		    }
+		});
 	}
 	
 	public void handleRollButton() {
+		try {
 		Random rand = new Random(); // dice roll and result output
-		int multiplier = (int) multiplierL.getUserData();
-		int modifier = (int) modifierL.getUserData();
-		int diceType = (int) diceTypeL.getUserData(); 
+		long multiplier = (long) multiplierL.getUserData();
+		long modifier = (long) modifierL.getUserData();
+		int diceType = (int) diceTypeL.getUserData();
 		long finalResult = 0;
-		for (int l = 0; l < multiplier; l++) {
-			int result = rand.nextInt(diceType) + 1;
+		for (long l = 0; l < multiplier; l++) {
+			long result = rand.nextInt(diceType) + 1;
 			finalResult = finalResult + result;
 		}
 		finalResult = finalResult + modifier;
 		rollResultLabel.setText("" + finalResult);
+		}catch(Exception e){
+			e.printStackTrace();
+			rollResultLabel.setText("Invalid parameters!");
+			String multiplier = multiplierL.getUserData().toString();
+			String modifier = modifierL.getUserData().toString();
+			String diceType = diceTypeL.getUserData().toString();
+			System.out.println(multiplier + " " + modifier + " " + diceType);
+		}
 	}
 	
 	public void handleMultiplierTF() {
 		try {
-			int multiplier = Integer.parseInt(multiplierTF.getText());
+			long multiplier = Long.parseLong(multiplierTF.getText());
 			if(multiplier<1) {
 				multiplierTF.setText("1");
 				multiplierL.setUserData(1);
@@ -75,19 +103,19 @@ public class DiceController extends GridPane{
 			}
 		}catch(Exception e){
 			multiplierTF.setText("1");
-			multiplierL.setUserData(1);
+			multiplierL.setUserData((long) 1);
 		}
 	}
 	
 	public void handleMultiplierPlusB() {
-		int multiplier = (Integer) multiplierL.getUserData();
+		long multiplier = (long) multiplierL.getUserData();
 		multiplier++;
 		multiplierTF.setText("" + multiplier);
 		multiplierL.setUserData(multiplier);
 	}
 	
 	public void handleMultiplierMinusB() {
-		int multiplier = (Integer) multiplierL.getUserData();
+		long multiplier = (long) multiplierL.getUserData();
 		if(multiplier>1) {
 			multiplier--;
 			multiplierTF.setText("" + multiplier);
@@ -97,29 +125,33 @@ public class DiceController extends GridPane{
 	
 	public void handleModifierTF() {
 		try {
-			int modifier = Integer.parseInt(modifierTF.getText());
+			long modifier = Long.parseLong(modifierTF.getText());
 			modifierL.setUserData(modifier);
 		}catch(Exception e){
-			modifierTF.setText("1");
-			modifierL.setUserData(1);
+			modifierTF.setText("0");
+			modifierL.setUserData((long) 0);
 		}
 	}
 	
 	public void handleModifierPlusB() {
-		int modifier = (Integer) modifierL.getUserData();
+		long modifier = (long) modifierL.getUserData();
 		modifier++;
 		modifierTF.setText("" + modifier);
 		modifierL.setUserData(modifier);
 	}
 	
 	public void handleModifierMinusB() {
-		int modifier = (Integer) modifierL.getUserData();
+		long modifier = (long) modifierL.getUserData();
 		modifier--;
 		modifierTF.setText("" + modifier);
 		modifierL.setUserData(modifier);
 	}
 	
 	public void handleToggleButtons() {
-		diceTypeL.setUserData(diceTypeTG.getSelectedToggle().getUserData());
+		if (diceTypeTG.getSelectedToggle()==null) {
+			diceTypeL.setUserData(0);
+		}else {
+			diceTypeL.setUserData(diceTypeTG.getSelectedToggle().getUserData());
+		}
 	}
 }
