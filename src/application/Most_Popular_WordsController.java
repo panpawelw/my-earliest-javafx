a59aea6fe329b1056dffedf1b527266e02799096
pjm77 <probably.just.me77@gmail.com>
2018-06-20 23:59:34 +0200

    handler added for Add Website button

--- src/application/Most_Popular_WordsController.java
+++ src/application/Most_Popular_WordsController.java
@@ -1,5 +1,25 @@
 package application;
 
-public class Most_Popular_WordsController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
+public class Most_Popular_WordsController extends BorderPane {
+	
+	@FXML public ScrollPane websitesPane;
+	@FXML public Button addButton;
+
+	@FXML
+	void initialize() {}
+	
+	public void handleAddButton() {
+		VBox websiteVBox = new VBox();
+		TextField websiteNameTF = new TextField();
+		TextField websiteSearchCriteriaTF = new TextField();
+		Button deleteButton = new Button();
+		
+	}
 }
