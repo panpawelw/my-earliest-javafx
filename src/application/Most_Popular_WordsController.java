58e14aa355420251cb98df09ba3342f2dc3f21e3
pjm77 <probably.just.me77@gmail.com>
2018-06-21 04:05:12 +0200

    mock handler added for Scan For Words button

--- src/application/Most_Popular_WordsController.java
+++ src/application/Most_Popular_WordsController.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -17,13 +22,27 @@ public class Most_Popular_WordsController extends BorderPane {
 	
 	public void handleAddButton() {
 		VBox websiteVBox = new VBox();
-		TextField websiteNameTF = new TextField();
+		TextField websiteNameTF = new TextField("add website...");
 		websiteNameTF.setPrefWidth(135);
-		TextField websiteSearchCriteriaTF = new TextField();
+		TextField websiteSearchCriteriaTF = new TextField("add search criteria...");
 		websiteSearchCriteriaTF.setPrefWidth(135);
-		Button deleteButton = new Button("Delete");
+		Button deleteButton = new Button("delete website");
 		deleteButton.setPrefWidth(135);
+		deleteButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				websitesVBox.getChildren().remove(websiteVBox);
+			}
+		});
 		websiteVBox.getChildren().addAll(websiteNameTF, websiteSearchCriteriaTF, deleteButton);
 		websitesVBox.getChildren().add(websiteVBox);
+	}
+	
+	public void handleScanButton() {
+		VBox currentVBox = (VBox) websitesVBox.getChildren().get(2);
+		TextField websiteNameTF = (TextField) currentVBox.getChildren().get(0);
+		System.out.println(websiteNameTF.getText());
+		TextField websiteSearchCriteriaTF = (TextField) currentVBox.getChildren().get(1);
+		System.out.println(websiteSearchCriteriaTF.getText());
 	}
 }
